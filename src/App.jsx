0727ea56815269461c04e763add0804262d7c74e
nghiep <nghiep010804@gmail.com>
2025-07-15 21:37:52 +0700

    them 2 bai nhac

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import './App.css'
-
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 function App() {
-
   return (
-    <>
-      <p>Hello NGHIEP</p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/video/:id" element={<Video />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
+function Home() {
+  return (
+    <>
 
-export default App
+    </>
+  );
+}
+function Video() {
+  const { id } = useParams();
+  const s = ["/mai_mai_khong_la_anh.mp4","/neu_ngay_ay.mp4"];
+  return (
+    <>
+      <video width="600" controls>
+        <source src={s[id]} type="video/mp4" />
+        Trình duyệt không hỗ trợ video.
+      </video>
+    </>
+  );
+}
+export default App;
